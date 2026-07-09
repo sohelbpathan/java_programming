102f5eca0ae853bc8875efbcdb276b9337bb2cf3
sohelbpathan <sohelbabulalpathan7@gmail.com>
2026-07-09 16:04:54 +0530

    Add files via upload

---
+++ Practice/LB Class/program43.java
@@ -0,0 +1,48 @@
+//type 2
+import java.util.Scanner;
+
+class program43
+{
+
+    public static boolean CheckDivisible(int iNo)
+    {
+         if((iNo%3==0) && (iNo%5==0))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+
+
+    }
+    public static void main(String A[])
+    {
+
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue=0;
+        boolean bRet =false;
+
+        System.out.println("Enter number :");
+        iValue =sobj.nextInt();
+
+        bRet = CheckDivisible(iValue); 
+       
+        if(bRet == true)
+        {
+            System.out.println("NUmber is divisible by 3 & 5");
+        }
+        else
+        {
+             System.out.println("NUmber is  not divisible by 3 & 5");
+
+        }
+
+    }
+ 
+}
+
+
+
---
+++ Practice/LB Class/program44.java
@@ -0,0 +1,53 @@
+//type 3
+
+import java.util.Scanner;
+
+class program44
+{
+    public static void main(String A[])
+    {
+
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue=0;
+        boolean bRet =false;
+
+        System.out.println("Enter number :");
+        iValue =sobj.nextInt();
+
+        bRet =NumberX.CheckDivisible(iValue); 
+       
+        if(bRet == true)
+        {
+            System.out.println("NUmber is divisible by 3 & 5");
+        }
+        else
+        {
+             System.out.println("NUmber is  not divisible by 3 & 5");
+
+        }
+
+    }
+ 
+}
+
+
+class NumberX
+{
+    public static boolean CheckDivisible(int iNo)
+    {
+        if((iNo % 3 ==0)&&(iNo % 5 ==0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+    }
+
+}
+
+
+
